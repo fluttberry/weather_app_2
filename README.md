682c9edca8b5a5eee453e3f1efe254478cfffd70
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-03-08 18:50:05 +0600

    weather app starts

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_app_2
+
+A new Flutter project.
---
+++ lib/ui/weather_home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WeatherHomePage extends StatefulWidget {
+  const WeatherHomePage({super.key});
+
+  @override
+  State<WeatherHomePage> createState() => _WeatherHomePageState();
+}
+
+class _WeatherHomePageState extends State<WeatherHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(children: [Image.asset('assets/graphic.png')]),
+      ),
+    );
+  }
+}
