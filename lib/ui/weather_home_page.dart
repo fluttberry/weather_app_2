1ec8db774d47cc3cdc76fc37247fcfc98d6cc78e
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-03-08 20:15:39 +0600

    ongoing

--- lib/ui/weather_home_page.dart
+++ lib/ui/weather_home_page.dart
@@ -11,8 +11,25 @@ class _WeatherHomePageState extends State<WeatherHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(children: [Image.asset('assets/graphic.png')]),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/graphic.png',
+            alignment: Alignment.topCenter,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          SizedBox(height: 800,), 
+          Container(
+            height: 531,
+            width: double.infinity,
+            // alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Colors.amber,
+            ),
+          ),
+        ],
       ),
     );
   }
