ef66f377f2bf140a4e1492d47a96989fb31ccfeb
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-03-19 11:23:46 +0600

    links updated

--- lib/repository/weather_repository.dart
+++ lib/repository/weather_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:weather_app_2/const/const.dart';
 import 'package:weather_app_2/model/current_weather_model.dart';
+import 'package:weather_app_2/model/forcast_weather_model.dart';
 
 class WeatherRepository {
  Future <CurrentWeatherModel?> getCurrentWeather (String cityName) async{
@@ -10,6 +11,15 @@ class WeatherRepository {
     var res = await http.get(Uri.parse('${Const.BaseUrl}/weather?q=$cityName&appid=${Const.ApiKey}&units=metric'));
     if (res.statusCode==200){
       return CurrentWeatherModel.fromJson(jsonDecode(res.body));
+    } else{
+      return null;
+    }
+  }
+   Future <ForcastWeatherModel?> getForcastWeather (String cityName) async{
+  print('---${Const.BaseUrl}/weather?q=$cityName&appid=${Const.ApiKey}&units=metric');
+    var res = await http.get(Uri.parse('${Const.BaseUrl}/forecast?q=$cityName&appid=${Const.ApiKey}&units=metric'));
+    if (res.statusCode==200){
+      return ForcastWeatherModel.fromJson(jsonDecode(res.body));
     } else{
       return null;
     }
